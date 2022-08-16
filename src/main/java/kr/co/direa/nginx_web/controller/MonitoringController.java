d274ae1bb40e9bf4c86d67c005e9cfafbbf8e2cc
daroguzo <kjw2122@naver.com>
2022-08-16 16:03:31 +0900

    add: nginx api 모니터링 서비스

--- src/main/java/kr/co/direa/nginx_web/controller/MonitoringController.java
+++ src/main/java/kr/co/direa/nginx_web/controller/MonitoringController.java
@@ -1,10 +1,17 @@
 package kr.co.direa.nginx_web.controller;
 
+import kr.co.direa.nginx_web.dto.OpenPort;
 import kr.co.direa.nginx_web.service.MonitoringService;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @RequestMapping(value = "/nginx/web")
 @Controller
@@ -17,10 +24,43 @@ public class MonitoringController {
         this.monitoringService = monitoringService;
     }
 
-    @GetMapping(value = "/test")
-    public String test() {
-        System.out.println(monitoringService.getNodeInfo());
-        return "index";
+    @GetMapping("/management")
+    public String getManagement(Model model) throws ParseException {
+
+        List<OpenPort> openPortList = monitoringService.getNodeStatus();
+
+        // nginx 서버가 기동중이 아니라면
+        if (openPortList.size() == 0) {
+            model.addAttribute("isRun", false);
+            return "management";
+        }
+
+        model.addAttribute("isRun", true);
+        model.addAttribute("openPortList", openPortList);
+        return "management";
     }
 
+    @PostMapping("/management/start")
+    public String startNginx(RedirectAttributes ra) {
+        monitoringService.startNginx();
+        ra.addFlashAttribute("result", "기동 완료!");
+        ra.addFlashAttribute("isRun", true);
+        return "redirect:/nginx/web/management";
+    }
+
+    @PostMapping("/management/stop")
+    public String stopNginx(RedirectAttributes ra) {
+        monitoringService.stopNginx();
+        ra.addFlashAttribute("result", "정지 완료!");
+        ra.addFlashAttribute("isRun", false);
+        return "redirect:/nginx/web/management";
+    }
+
+    @PostMapping("/management/reload")
+    public String reloadNginx(RedirectAttributes ra) {
+        monitoringService.reloadNginx();
+        ra.addFlashAttribute("result", "재기동 완료!");
+        ra.addFlashAttribute("isRun", true);
+        return "redirect:/nginx/web/management";
+    }
 }
--- src/main/java/kr/co/direa/nginx_web/service/MonitoringService.java
+++ src/main/java/kr/co/direa/nginx_web/service/MonitoringService.java
@@ -1,28 +1,68 @@
 package kr.co.direa.nginx_web.service;
 
+import kr.co.direa.nginx_web.dto.OpenPort;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MonitoringService {
 
     @Value("${node1.nginx_api.ip}")
-    private String ip;
+    private String node1Ip;
 
     @Value("${node1.nginx_api.port}")
-    private int port;
+    private int node1Port;
 
-    public String getNodeInfo() {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://192.168.1.200:8888" + "/nginx/api/monitoring", String.class);
-        return responseEntity.getBody();
+    // 해당 노드 nginx 정보 가져오기
+    public List<OpenPort> getNodeStatus() throws ParseException {
+        String getBody = getHttpMethod(node1Ip, node1Port, "/nginx/api/monitoring");
+        List<OpenPort> openPortList = new ArrayList<>();
+
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(getBody);
+        // nginx 기동 검사
+        if (jsonObject.get("run").equals(false)) {
+            return openPortList;
+        }
+
+        // 열려있는 포트 목록 조회
+        JSONArray list = (JSONArray) jsonObject.get("openPortList");
+        for (Object o : list) {
+            JSONObject ipPort = (JSONObject) o;
+            OpenPort openPort = OpenPort.builder()
+                    .ip(ipPort.get("ip").toString())
+                    .port((long)ipPort.get("port"))
+                    .build();
+            openPortList.add(openPort);
+        }
+
+        // nginx 구동중?
+        return openPortList;
+    }
+    public void startNginx() {
+        getHttpMethod(node1Ip, node1Port, "/nginx/api/start");
+    }
+    public void stopNginx() {
+        getHttpMethod(node1Ip, node1Port, "/nginx/api/stop");
+    }
+    public void reloadNginx() {
+        getHttpMethod(node1Ip, node1Port, "/nginx/api/reload");
     }
 
-    public String getHttpMethod(String url) {
+
+    // get http body
+    private String getHttpMethod(String ip, int port, String url) {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://192.168.1.200:8888" + "/nginx/api/monitoring", String.class);
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://" + ip + ":"+ port + url, String.class);
         return responseEntity.getBody();
     }
 }
